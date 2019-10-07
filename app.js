b13637fc296dfe7ebec786c799bf083cb230d3fd
Ivan P <gutehaltung@gmail.com>
2019-10-07 15:08:48 +0300

    Changed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const hbs = require('hbs')
 const app = express()
-const port = process.env.PORT || 5003
+const port = process.env.PORT || 3000
 
 const mongoConnect = require('./models/mongoConnect')
 
