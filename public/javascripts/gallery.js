27d54836cd269bfd7eb9002f52708ce0d471c101
Ivan P <gutehaltung@gmail.com>
2019-10-09 17:56:29 +0300

    Added HTML-minifier

--- public/javascripts/gallery.js
+++ public/javascripts/gallery.js
@@ -1,7 +1,12 @@
-const bigImage = document.querySelector('#bigImage picture')
+const bigImage = document.querySelectorAll('#bigImage picture')
 const gallery = document.getElementById('gallery')
 
-gallery.addEventListener('click', evt => {
-	bigImage.firstElementChild.srcset = evt.target.previousElementSibling.srcset // Change srcset in source
-	bigImage.lastElementChild.src = evt.target.src // Change src in img
+console.log(gallery.length)
+
+bigImage[0].classList.add('shown')
+gallery.addEventListener('click', ev => {
+	bigImage.forEach(element => {
+		element.classList.remove('shown')
+		if (ev.target.attributes.index.value == element.id) element.classList.add('shown')
+	})
 })
