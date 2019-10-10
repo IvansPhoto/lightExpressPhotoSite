8a3a647ef0395a2e123b2db2340d6fbcacca2f94
Ivan P <gutehaltung@gmail.com>
2019-10-10 16:00:53 +0300

    Returned CSS as a separate file

--- routes/portraits.js
+++ routes/portraits.js
@@ -7,7 +7,7 @@ const photosModel = require('./../models/photosModel')
 router.get('/', async (req, res) => {
 	try {
 		const photos = await photosModel.find({})
-		res.render('portraits', {title: 'Portraits', photos: photos, metaTags: "portraits, girls, photos"})
+		res.render('portraits', {title: 'Portraits', pageStyle: 'portraits', photos: photos, metaTags: "portraits, girls, photos"})
 	} catch (e) {
 		console.error(e)
 		res.redirect('/')
