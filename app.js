ac9a575c464054409ff2297cf7a64cad9ea7eb98
Ivan P <gutehaltung@gmail.com>
2019-10-18 19:25:02 +0300

    Added error handler

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const createError = require('http-errors')
 const hbs = require('hbs')
 const path = require('path')
 const compression = require('compression')
@@ -56,6 +57,23 @@ app.use('/', require('./routes/index'))
 app.use('/portraits', require('./routes/portraits'))
 app.use('/portraits/', require('./routes/phset'))
 
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+	next(createError(404))
+})
+
+// error handler
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message
+	res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+	// render the error page
+	res.status(err.status || 500)
+	res.render('error')
+})
 
 app.listen(PORT, err => {
 	console.log(`Listen on ${PORT}`)
