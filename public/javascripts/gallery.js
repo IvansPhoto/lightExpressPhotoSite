0e093adf6322b816c83f087d26edb35a8a850c18
Ivan P <gutehaltung@gmail.com>
2019-10-18 14:11:47 +0300

    Added animation to the gallery

--- public/javascripts/gallery.js
+++ public/javascripts/gallery.js
@@ -1,12 +1,23 @@
-const bigImage = document.querySelectorAll('#bigImage picture')
+const bigSource = document.querySelector('#bigImage source')
+const bigImage = document.querySelector('#bigImage img')
 const gallery = document.getElementById('gallery')
 
-console.log(gallery.length)
 
-bigImage[0].classList.add('shown')
 gallery.addEventListener('click', ev => {
-	bigImage.forEach(element => {
-		element.classList.remove('shown')
-		if (ev.target.attributes.index.value == element.id) element.classList.add('shown')
-	})
+	const imgNumber = ev.target.attributes.imgNumber.value // Get number of an image from gallery
+	const imgFolder = ev.target.attributes.folder.value // Get a folder name of an image from gallery
+
+	bigImage.classList.toggle('opacity-1') // For animation - remove "opacity: 1"
+	bigImage.classList.toggle('opacity-0') // For animation - add "opacity: 1"
+
+	setTimeout(() => {
+		bigSource.srcset = `/images/${imgFolder}/${imgNumber}_480.webp 480w, /images/${imgFolder}/${imgNumber}_960.webp 960w, /images/${imgFolder}/${imgNumber}_1920.webp 1920w`
+		bigImage.src = `/images/${imgFolder}/${imgNumber}_1920.jpg`
+	}, 250)
+
+	setTimeout(() => {
+		bigImage.classList.toggle('opacity-1')
+		bigImage.classList.toggle('opacity-0')
+	}, 350)
+
 })
