5005bbc121c03b0296990702a206a2d5c46643e4
Ivan P <gutehaltung@gmail.com>
2019-10-21 23:33:17 +0300

    Changed mongoURL

--- models/mongoConnect.js
+++ models/mongoConnect.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
-const config = require('config')
-const mongoUrl = config.get('mongoUrl')
+// const config = require('config')
+// const mongoUrl = config.get('mongoUrl')
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/photoSite'
 
 module.exports = connectDB = mongoose.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
 	.then((e) => console.log(`Connected to ${e.connections[0].host}:${e.connections[0].port}`))
